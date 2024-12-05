1e8e6920921d75936e6ea1032bf3743076562275
Graham Arnett <GrahamKentArnett@outlook.com>
2024-12-05 14:57:21 -0500

    init commit

---
+++ TriangleCalcApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TriangleCalcApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my equilateral triangle calculator!");
+            string choice;
+            do
+            {
+                
+                    Console.Write("Enter a number for the sides: ");
+                  double side = double.Parse(Console.ReadLine());
+                
+                double perimeter = side * 3;
+
+                double area = (side * side) / 2;
+
+                Console.WriteLine($"Perimeter of the equilateral triangle is: {perimeter}" +
+                    $"\nArea of the equilateral triangle is: {area}");
+
+                Console.Write("Would you like to go again? (y/n): ");
+                choice = Console.ReadLine().ToLower();
+            }while(choice == "y");
+        }
+    }
+}
